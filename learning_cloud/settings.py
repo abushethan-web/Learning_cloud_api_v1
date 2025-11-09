b6abd3b98169306dfe83396c658630530b78285b
Ethan313241 <amenguda@gmail.com>
2025-11-09 18:54:17 +0300

    remove rate limit

--- learning_cloud/settings.py
+++ learning_cloud/settings.py
@@ -59,6 +59,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     'rest_framework',
+    'rest_framework.authtoken',  # For Token authentication
     'corsheaders',
     'oauth2_provider',
     'django_filters',
